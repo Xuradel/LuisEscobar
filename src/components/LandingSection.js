25d854d0305582a9c626e8648c549d4867c9863e
Xuradel <leagueofxuradel@gmail.com>
2023-04-15 19:42:24 -0600

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,39 @@
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+ 
+const greeting = "Hello, I am Luis!"; 
+const bio1 = "A frontend developer"; 
+const bio2 = "specialized in React"; 
+ 
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar id="avatar"
+         src="https://media.licdn.com/dms/image/D4E03AQHjVVie2aVQng/profile-displayphoto-shrink_800_800/0/1675815517347?e=1687392000&v=beta&t=-sMpF7L_x0upETaM6pJol361I1010NSRamjr_pakc1Q"
+         size="2xl" 
+         name="Luis Escobar" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1}> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio1} 
+       </Heading> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading> 
+     </VStack> 
+   </VStack> 
+ </FullScreenSection> 
+); 
+ 
+export default LandingSection;
---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,68 @@
+import React from "react"; 
+import FullScreenSection from "./FullScreenSection"; 
+import { Box, Heading } from "@chakra-ui/react"; 
+import Card from "./Card"; 
+ 
+const projects = [ 
+ { 
+   title: "Organic Coffee Shop", 
+   description: 
+     "Landing page for the Organic Coffee Shop, made with HTML5, CSS using a combination of flex and grid.", 
+   getImageSrc: () => require("../images/photo1.jpg"), 
+   url: "https://google.com",
+ }, 
+ { 
+   title: "Little Lemon Restaurant", 
+   description: 
+     "Landing page for the Little Lemon Restaurant, made with HTML5, CSS using flexbox.", 
+   getImageSrc: () => require("../images/photo2.jpg"), 
+   url: "https://facebook.com",
+ }, 
+ { 
+   title: "Photo Gallery", 
+   description: 
+     "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income", 
+   getImageSrc: () => require("../images/photo3.jpg"), 
+   url: "https://instagram.com",
+ }, 
+ { 
+   title: "Event planner", 
+   description: 
+     "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps", 
+   getImageSrc: () => require("../images/photo4.jpg"), 
+   url: "https://youtube.com",
+ }, 
+]; 
+ 
+const ProjectsSection = () => { 
+ return ( 
+   <FullScreenSection 
+     backgroundColor="#14532d" 
+     isDarkBackground 
+     p={8} 
+     alignItems="flex-start" 
+     spacing={8} 
+   > 
+     <Heading as="h1" id="projects-section"> 
+       My Work 
+     </Heading> 
+     <Box 
+       display="grid" 
+       gridTemplateColumns="repeat(2,minmax(0,1fr))" 
+       gridGap={8} 
+     > 
+       {projects.map((project) => ( 
+         <Card 
+           key={project.title} 
+           title={project.title} 
+           description={project.description} 
+           url={project.url}
+           imageSrc={project.getImageSrc()} 
+         /> 
+       ))} 
+     </Box> 
+   </FullScreenSection> 
+ ); 
+}; 
+ 
+export default ProjectsSection;
